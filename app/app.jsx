2f9f6b27567bea2784d563f59880ab98b3b33b2a
Jay Sanders <j.sanders@realitychecksystems.com>
2017-03-14 16:37:43 -0700

    Initial commit

---
+++ app/app.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+
+require('style-loader!css-loader!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
+require('style-loader!css-loader!sass-loader!applicationStyles');
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+        </Route>
+    </Router>,
+    document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'script-loader!jquery/dist/jquery.min.js',
+        'script-loader!foundation-sites/dist/foundation.min.js',
+        './app/app.jsx'
+    ],
+    externals: {
+        jquery: 'jQuery'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            '$': 'jquery',
+            'jQuery': 'jquery'
+        })
+    ],
+    output: {
+        path: __dirname,
+        filename: './public/bundle.js'
+    },
+    resolve: {
+        modules: [
+            __dirname, 'node_modules'
+        ],
+        alias: {
+            Main: 'app/components/Main.jsx',
+            applicationStyles: 'app/styles/app.scss'
+        },
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0']
+                },
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/
+            }
+        ]
+    },
+    devtool: 'cheap-module-eval-source-map'
+};
